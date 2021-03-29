23e897f8557cd2a7ac2cb49dd740677b6e9c0f56
Klem26 <Kankina26@gmail.com>
2021-03-29 18:03:52 +0300

    создала отдельный файл для ссылок

---
+++ goit-js-hw-11-color-switch/src/data/refs.js
@@ -0,0 +1,9 @@
+
+  const refs = {
+    start: document.querySelector('[data-action="start"]'),
+    stop: document.querySelector('[data-action="stop"]'),
+    body: document.body
+   
+  };
+
+  export default (refs)
